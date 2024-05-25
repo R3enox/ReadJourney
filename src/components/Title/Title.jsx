d6918062b08e5e01957444c78b961ea494ce1a38
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-05-25 15:37:46 +0300

    change path
    change path

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,4 +1,4 @@
-import { TitleWrapper } from './Title.Styled';
+import { TitleWrapper } from './Title.styled';
 
 export const Title = ({ title }) => {
   return <TitleWrapper className="title">{title}</TitleWrapper>;
